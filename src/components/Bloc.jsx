d4ef405a8ed266b8c95d78e0ea3b4284ef09cc21
ErwinAdriel <adrielquispe87@gmail.com>
2024-05-14 19:18:06 -0300

    Probando useState

--- src/components/Bloc.jsx
+++ src/components/Bloc.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 
 
 export default function Bloc(){
+
+    const [title, setTitle] = useState("Hola");
+
+    function handleClick(e){
+        e.preventDefault();
+        setTitle('Adriel');
+    }
     return(
         <div className="container">
             <form className="form">
-                <input className="input"></input>
-                <input type="submit" value="Guardar" className="buttonGuardar"></input>
+                <input className="input" value={title}></input>
+                <input
+                onClick={handleClick}
+                type="submit" 
+                value="Guardar" 
+                className="buttonGuardar">
+                </input>
             </form>
         </div>
         
